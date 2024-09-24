a3c82e058926fd2841aa954da5aad20a129c8c6f
turriettaa <164412880+turriettaa@users.noreply.github.com>
2024-09-23 21:33:57 -0700

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,42 +1,36 @@
 import React from 'react';
 import { ApolloClient, InMemoryCache, ApolloProvider, createHttpLink } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+import { Outlet } from 'react-router-dom';
 import Navbar from './components/Navbar';
+
 // Create an HTTP link
 const httpLink = createHttpLink({
   uri: '/graphql',
 });
+
 // Create a middleware for authentication
 const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
     headers: {
       ...headers,
-      authorization: token ? <code>Bearer ${token}</code> : '',
+      authorization: token ? `Bearer ${token}` : '',
     },
   };
 });
+
 // Create Apollo Client
 const client = new ApolloClient({
   link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
+
 function App() {
   return (
     <ApolloProvider client={client}>
-      <Router>
-        <>
-          <Navbar />
-          <Routes>
-            <Route path='/' element={<SearchBooks />} />
-            <Route path='/saved' element={<SavedBooks />} />
-            <Route render={() => <h1>Wrong page!</h1>} />
-          </Routes>
-        </>
-      </Router>
+      <Navbar />
+      <Outlet />
     </ApolloProvider>
   );
 }
